490976a8b3033a7a87c872885e8c2bf0793f2ac8
Diark <25520458@gm.uit.edu.vn>
2026-02-12 21:45:04 +0700

    LeetCode 75

---
+++ LeetCode/Problems/1431.kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1431 lang=cpp
+ *
+ * [1431] Kids With the Greatest Number of Candies
+ */
+// @lc code=start
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxcandies = 0;
+        for(int i =0;i<candies.size();i++){
+            if(candies[i] > maxcandies) maxcandies = candies[i];
+        }
+        vector<bool> kq;
+        for(int i=0;i<candies.size();i++){
+            bool check = (candies[i] + extraCandies >= maxcandies);
+            kq.push_back(check);
+        }
+        return kq;
+    }
+};
+// @lc code=end
+

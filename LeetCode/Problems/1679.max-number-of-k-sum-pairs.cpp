17de90399a0672c56087edf600935b8c6f6bbd5f
Diark <25520458@gm.uit.edu.vn>
2026-02-14 23:43:44 +0700

    LeetCode 75

--- LeetCode/Problems/1679.max-number-of-k-sum-pairs.cpp
+++ LeetCode/Problems/1679.max-number-of-k-sum-pairs.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        
+        std::sort(nums.begin(),nums.end());
+        int left = 0, right = nums.size() -1;
+        int count =0;
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if (sum > k) right--;
+            else if (sum < k) left++;
+            else {
+                right--;
+                left++;
+                count++;
+            }
+        }
+        return count;
     }
 };
 // @lc code=end

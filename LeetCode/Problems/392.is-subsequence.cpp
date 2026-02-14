06f8e7e463598d5e154ea5572ddc64525beb6259
Diark <25520458@gm.uit.edu.vn>
2026-02-14 14:50:14 +0700

    LeetCode 75

--- LeetCode/Problems/392.is-subsequence.cpp
+++ LeetCode/Problems/392.is-subsequence.cpp
@@ -9,7 +9,7 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i = 0, j=0;
-        while(j<t.size()){
+        while(j<t.size() && i < s.size()){
             if(t[j] == s[i]) i++;
             j++;
         }

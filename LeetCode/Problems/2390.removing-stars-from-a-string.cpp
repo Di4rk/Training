ce1ea65039ea0c71dfc70b5b27008a31cb60a279
Diark <25520458@gm.uit.edu.vn>
2026-02-17 22:19:12 +0700

    LeetCode 75

---
+++ LeetCode/Problems/2390.removing-stars-from-a-string.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=2390 lang=cpp
+ *
+ * [2390] Removing Stars From a String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeStars(string s) {
+        string kq;
+        for(char x : s){
+                        if(x=='*') {
+                kq.pop_back();
+                continue;
+            }
+            kq.push_back(x);
+        }
+        return kq;
+    }
+};
+// @lc code=end
+

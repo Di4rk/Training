7614d7bc7bc757a12693784ff697d424f206770b
Diark <25520458@gm.uit.edu.vn>
2026-02-18 14:05:48 +0700

    LeetCode 75

--- LeetCode/Problems/394.decode-string.cpp
+++ LeetCode/Problems/394.decode-string.cpp
@@ -8,49 +8,32 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<string> chuoiDaLuu; // Lưu chuỗi phía trước dấu '['
-        stack<int> soLanLap;      // Lưu số k
-        string chuoiHienTai = ""; 
-        int k = 0;
-
-        for (char c : s) {
-            // 1. Nếu là số (xử lý cả trường hợp số có nhiều chữ số như 10, 100...)
-            if (isdigit(c)) {
-                k = k * 10 + (c - '0');
+        stack<string> strStored;
+        stack<int> time;
+        string kq = "";
+        int k=0;
+        for(char c : s){
+            if(isdigit(c)){
+                k = k *10 + (c - '0');
             }
-            // 2. Nếu là dấu '[' -> Bắt đầu một đoạn mã hóa mới
-            else if (c == '[') {
-                soLanLap.push(k);           // Cất số k vào kho
-                chuoiDaLuu.push(chuoiHienTai); // Cất chuỗi đang làm dở vào kho
-                
-                // Reset để bắt đầu xử lý đoạn trong ngoặc []
-                k = 0;
-                chuoiHienTai = "";
+            else if(c == '['){
+                time.push(k);
+                strStored.push(kq);
+                k=0;
+                kq="";
             }
-            // 3. Nếu là dấu ']' -> Kết thúc đoạn mã hóa, bung lụa
-            else if (c == ']') {
-                string chuoiTam = chuoiHienTai;
-                
-                // Lấy số lần lặp từ kho ra
-                int count = soLanLap.top(); 
-                soLanLap.pop();
-                
-                // Lấy cái chuỗi cũ (trước dấu [) ra
-                string chuoiCu = chuoiDaLuu.top(); 
-                chuoiDaLuu.pop();
-                
-                // Nối chuỗi cũ với chuỗi trong ngoặc được lặp lại k lần
-                chuoiHienTai = chuoiCu;
-                while (count--) {
-                    chuoiHienTai += chuoiTam;
-                }
+            else if (c== ']'){
+                string sub = kq;
+                int count = time.top();
+                time.pop();
+                string oldStr = strStored.top();
+                strStored.pop();
+                kq = oldStr;
+                while(count--) kq += sub;
             }
-            // 4. Nếu là chữ cái bình thường
-            else {
-                chuoiHienTai += c;
-            }
+            else kq += c;
         }
-        return chuoiHienTai;
+        return kq;
     }
 };
 // @lc code=end

d3e56a347f6358daeb9286c9f52e50078f8283ab
Diark <25520458@gm.uit.edu.vn>
2026-02-21 20:12:47 +0700

    LeetCode 75

--- LeetCode/Problems/872.leaf-similar-trees.cpp
+++ LeetCode/Problems/872.leaf-similar-trees.cpp
@@ -18,28 +18,18 @@
  */
 class Solution {
 public:
-    // Hàm trinh sát: Đi sâu xuống tận cùng để nhặt lá
-    void getLeaves(TreeNode* node, vector<int>& leaves) {
-        if (node == nullptr) return;
-        
-        // Cú chốt: Nếu node này không có con trái & phải -> Chính là lá!
-        if (node->left == nullptr && node->right == nullptr) {
+    void getLeaves(TreeNode* node, vector<int>& leaves){
+        if(node == nullptr) return;
+        if(node->left == nullptr && node->right== nullptr){
             leaves.push_back(node->val);
         }
-        
-        // Bắt buộc rẽ trái trước, sau đó mới rẽ phải để đúng thứ tự left-to-right
         getLeaves(node->left, leaves);
         getLeaves(node->right, leaves);
     }
-
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         vector<int> leaves1, leaves2;
-        
-        // Cho đệ quy đi gom lá của cả 2 cây
         getLeaves(root1, leaves1);
         getLeaves(root2, leaves2);
-        
-        // Phép thuật của C++: So sánh 2 mảng trực tiếp luôn
         return leaves1 == leaves2;
     }
 };

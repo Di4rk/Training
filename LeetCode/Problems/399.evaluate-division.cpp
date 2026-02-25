39afe235337cb950983cf36e633ffcd3c88b6d8e
Diark <25520458@gm.uit.edu.vn>
2026-02-25 23:48:10 +0700

    LeetCode 75

---
+++ LeetCode/Problems/399.evaluate-division.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=399 lang=cpp
+ *
+ * [399] Evaluate Division
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        int qth;
+    }
+};
+// @lc code=end
+

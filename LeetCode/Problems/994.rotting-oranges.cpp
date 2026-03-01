9827ff8e8f15eeedd6b7763f5a2947001ac3a9d4
Diark <25520458@gm.uit.edu.vn>
2026-03-02 00:01:23 +0700

    LeetCode 75

--- LeetCode/Problems/994.rotting-oranges.cpp
+++ LeetCode/Problems/994.rotting-oranges.cpp
@@ -14,33 +14,31 @@ public:
         int fresh_count = 0;
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(grid[i][j] == 2) q.push_back({i,j});
+                if(grid[i][j] == 2) q.push({i,j});
                 else if (grid[i][j]==1) fresh_count++;
             }
         }
-    if (!fresh_count) return 0;
-    vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
-    int minutes = 0;
-    while(!q.empty() && fresh_count>0){
-        int current_q_size = q.size();
-        for(int i = 0; i< current_q_size;i++){
-            auto [r,c] = q.front();
-            q.pop();
-            for(auto dir : directions){
-                int nextr = r + dir.first;
-                int nextc = c + dir.sencond;
-                if(nextr>=0 && nextr < m && nextc >= 0 && nextc < n && gird[nextr][nextc] == 1){
-                    q.push_back({nextr,nextc});
-                    gird[nextr][nextc] == 2;
-                    fresh_count--;
-                }                
+        if (!fresh_count) return 0;
+        vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        int minutes = 0;
+        while(!q.empty() && fresh_count>0){
+            int current_q_size = q.size();
+            for(int i = 0; i< current_q_size;i++){
+                auto [r,c] = q.front();
+                q.pop();
+                for(auto dir : directions){
+                    int nextr = r + dir.first;
+                    int nextc = c + dir.sencond;
+                    if(nextr>=0 && nextr < m && nextc >= 0 && nextc < n && gird[nextr][nextc] == 1){
+                        q.push({nextr,nextc});
+                        grid[nextr][nextc] == 2;
+                        fresh_count--;
+                    }                
+                }
             }
+            minutes++;
         }
-        minutes++;
+        return fresh_count == 0 ? minutes : -1;
     }
-    return fresh_count == 0 ? minutes : -1;
-}
 };
 // @lc code=end
-
-asda

ca384e8900a4dbfb280fe2c600eb4524a02e7733
Diark <25520458@gm.uit.edu.vn>
2026-03-01 23:51:39 +0700

    LeetCode 75

--- LeetCode/Problems/994.rotting-oranges.cpp
+++ LeetCode/Problems/994.rotting-oranges.cpp
@@ -8,7 +8,33 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh_count = 0;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(gird[i][j] == 2) q.push_back({i,j});
+                else if (grid[i][j]==1) fresh_count++;
+            }
+        }
+    }
+    if (!fresh_count) return 0;
+    vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
+    int minutes = 0;
+    while(!q.empty() && fresh_count>0){
+        int current_q_size = q.size();
+        for(int i = 0; i< current_q_size;i++){
+            auto [r,c] = q.front();
+            q.pop();
+            for(auto dir : directions){
+                int nextr = r + dir.first;
+                int nextc = c + dir.sencond;
+                if(nextr>=0 && nextr < m && nextc >= 0 && nextc < n && gird[nextr][nextc] == 1){
+                    q.push_back({nextr,nextc});
+                }
+            }
+        }
     }
 };
 // @lc code=end

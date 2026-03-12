0debfc49781945ef5c86a7d4556601329c410faa
Diark <25520458@gm.uit.edu.vn>
2026-03-12 23:29:33 +0700

    LeetCode 75

--- LeetCode/Problems/2542.maximum-subsequence-score.cpp
+++ LeetCode/Problems/2542.maximum-subsequence-score.cpp
@@ -8,6 +8,19 @@
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+
+        vector<pair<int,int>> pairs(n);
+        for(int i = 0; i < n; i++){
+            pairs[i] = {nums2[i],nums1[i]};
+        }
+
+        sort(pairs.rbegin(), pairs.rend());
+
+        priority_queue <int, vector<int>, greater<int>> minHeap;
+        long long currentSum = 0;
+        long long maxScore;
+
         
     }
 };

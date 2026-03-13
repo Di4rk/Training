fe618b928f5e66aa1e166c22aa019666ce798507
Diark <25520458@gm.uit.edu.vn>
2026-03-13 13:50:14 +0700

    LeetCode 75

--- LeetCode/Problems/374.guess-number-higher-or-lower.cpp
+++ LeetCode/Problems/374.guess-number-higher-or-lower.cpp
@@ -17,21 +17,26 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        int left = 0;
-        int right = n-1;
-        int mid = left + (right - left)/2;
-        int result = guess(mid);
-        while(result != 0){
-            mid = left + (right - left)/2;
-            result = guess(mid);
+        int left = 1;
+        int right = n;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            int result = guess(mid);
+            
+            if(result == 0){
+                return mid;
+            }
+
             if(result == 1){
                 left = mid + 1;
             }
-            if(result == -1){
+            else {
                 right = mid - 1;
             }
         }        
-        return mid;
+
+        return -1;
     }
 };
 // @lc code=end

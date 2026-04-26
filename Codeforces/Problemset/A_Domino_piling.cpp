13740dcbbdd91007ded9fc9b05d0bb1a0b7d7aba
Diark <25520458@gm.uit.edu.vn>
2026-04-27 01:16:13 +0700

    Day 3

---
+++ Codeforces/Problemset/A_Domino_piling.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m, n;
+    cin >> m >> n;
+    int sum = m * n;
+    cout << sum /2;
+    return 0;
+}

5754db8081350a0ab64382d099e5e3e9ce5cde01
Diark <25520458@gm.uit.edu.vn>
2026-04-26 18:34:27 +0700

    Day 2

---
+++ Codeforces/Problemset/A_Stones_on_the_Table.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    char *s = new char[n];
+    int cnt = 0;
+    for(int i = 0; i < n; ++i) {
+        cin >> s[i];
+        if(i != 0 && s[i-1] == s[i]) cnt++;
+    }
+    cout << cnt;
+    return 0;
+
+
+}

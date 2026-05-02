3b7aa2dd2a5930a95f992983f9ecfb213a150f00
Diark <25520458@gm.uit.edu.vn>
2026-05-02 16:42:03 +0700

    Day 4

---
+++ Codeforces/Problemset/A_Wrong_Subtraction.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+    while(k--){
+        if(n%10 != 0){
+            --n;
+        } 
+        else {
+            n/=10;
+        }
+    }
+    cout << n;
+    return 0;
+}
